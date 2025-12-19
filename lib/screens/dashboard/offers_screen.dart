e5e288668e364e988e773c56973ac18d748cd9c7
Amisha Basnet <amishaaaaaa045@gmail.com>
2025-12-19 10:51:59 +0545

    searchScreen Updated

--- lib/screens/dashboard/offers_screen.dart
+++ lib/screens/dashboard/offers_screen.dart
@@ -5,6 +5,13 @@ class OffersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Offers Screen',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/dashboard/search_screen.dart
+++ lib/screens/dashboard/search_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:booko/widgets/movie_list_item.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Search Screen',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
   }
 }
